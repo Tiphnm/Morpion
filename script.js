1246f1df5f9f552444486f6e58d770da2a616bfb
Tiphnm <76214478+Tiphnm@users.noreply.github.com>
2021-02-02 17:16:38 +0100

    Division en 2 pages et message sur page d'accueil

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+////////////////////////////////////////////////HOMEPAGE/////////////////////////////////////////
+document.querySelector("#enter").addEventListener("click", function(){ 
+    let playerName = document.querySelector("#Player_1_name").value;
+    console.log(playerName);
+    document.getElementById("Player_1_info").innerText = "X Player " + playerName + " is ready!";
+    document.getElementById("Player_1_form").style.display = "none";
+})
+
+document.querySelector("#enter_2").addEventListener("click", function(){ 
+    let playerName = document.querySelector("#Player_2_name").value;
+    console.log(playerName);
+    document.getElementById("Player_2_info").innerText = "O Player " + playerName + " is ready!";
+    document.getElementById("Player_2_form").style.display = "none";
+})
+
+
+////////////////////////////////////////////////GAME/////////////////////////////////////////////
 const signX = "X"
 const signO = "O"
 var compteur = 0 
